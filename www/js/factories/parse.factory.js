29ea54f8dbddd28b2c612cb5880c2047d6119151
J.Coleman <jcoleman@dovico.com>
2017-12-04 12:17:15 -0400

    change regions

--- www/js/factories/parse.factory.js
+++ www/js/factories/parse.factory.js
@@ -785,22 +785,22 @@ angular.module('garago.factory.parse', [])
           }
         }).then(function(res){
           var regions = [{
-            id: 0,
+            id: "0",
             title: "Bathurst"
           },{
-            id: 1,
+            id: "1",
             title: "Fredericton"
           },{
-            id: 2,
+            id: "2",
             title: "Saint-Jean"
           },{
-            id: 3,
+            id: "3",
             title: "Moncton"
           },{
-            id: 4,
+            id: "4",
             title: "Edmundson"
           },{
-            id: 5,
+            id: "5",
             title: "Péninsule Acadienne"
           }]
 
